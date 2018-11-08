327f27c7dc9292ff0ee72baaae926190b5620f8d
fenlan <1475307818@qq.com>
2018-11-08 22:10:51 +0800

    modify default data

--- security.sql
+++ security.sql
@@ -1,4 +1,2 @@
-USE test;
-INSERT INTO `sys_role`(`id`,`name`) VALUES (1,''ROLE_ADMIN''),(2,''ROLE_USER'');
-INSERT INTO `sys_user`(`id`,`password`,`username`) VALUES (1,''root'',''root''),(2,''sang'',''sang'');
-INSERT INTO `sys_user_roles`(`sys_user_id`,`roles_id`) VALUES (1,1),(2,2);
+use test;
+insert  into `sys_role`(`name`) values ('ROLE_ADMIN'),('ROLE_USER');
