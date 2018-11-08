ee1988c87cb60b36ccc9ac79774eee147d575710
fenlan <1475307818@qq.com>
2018-11-08 22:06:53 +0800

    add remember-me config bean

--- src/main/java/com/fenlan/spring/springsecurity/security/config/WebSecurityConfig.java
+++ src/main/java/com/fenlan/spring/springsecurity/security/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.fenlan.spring.springsecurity.security.config;
 
 import com.fenlan.spring.springsecurity.security.service.CustomUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.RememberMeAuthenticationProvider;
@@ -11,14 +13,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("dataSource")
+    DataSource dataSource;
+
     @Bean
     UserDetailsService customUserService() {
         return new CustomUserService();
@@ -30,23 +41,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public TokenBasedRememberMeServices tokenBasedRememberMeServices() {
-        TokenBasedRememberMeServices services = new TokenBasedRememberMeServices("TEST", customUserService());
-        services.setTokenValiditySeconds(60*60*2);
-        return services;
-    }
+//    @Bean
+//    public TokenBasedRememberMeServices tokenBasedRememberMeServices() {
+//        TokenBasedRememberMeServices services = new TokenBasedRememberMeServices("TEST", customUserService());
+//        services.setTokenValiditySeconds(60*60*2);
+//        return services;
+//    }
 
     @Bean
     public RememberMeAuthenticationProvider rememberMeAuthenticationProvider() {
-        RememberMeAuthenticationProvider rmap = new RememberMeAuthenticationProvider("TEST");
-        return rmap;
+        return new RememberMeAuthenticationProvider("TEST");
     }
 
     @Bean
     public RememberMeAuthenticationFilter rememberMeAuthenticationFilter() throws Exception {
-        RememberMeAuthenticationFilter myFilter = new RememberMeAuthenticationFilter(authenticationManagerBean(), tokenBasedRememberMeServices());
-        return myFilter;
+        return new RememberMeAuthenticationFilter(authenticationManager(), rememberMeServices());
+    }
+
+    @Bean
+    public RememberMeServices rememberMeServices() {
+        JdbcTokenRepositoryImpl rememberMeTokenRepository = new JdbcTokenRepositoryImpl();
+        // 此处需要设置数据源，否则无法从数据库查询验证信息
+        rememberMeTokenRepository.setDataSource(dataSource);
+
+        // 此处的 key 可以为任意非空值(null 或 "")，单必须和起前面
+        // rememberMeServices(RememberMeServices rememberMeServices).key(key)的值相同
+        PersistentTokenBasedRememberMeServices rememberMeServices =
+                new PersistentTokenBasedRememberMeServices("TEST", customUserService(), rememberMeTokenRepository);
+
+        // 该参数不是必须的，默认值为 "remember-me", 但如果设置必须和页面复选框的 name 一致
+        rememberMeServices.setParameter("remember-me");
+        return rememberMeServices;
     }
 
     @Override
@@ -57,7 +82,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .logout().and()
-                .rememberMe().key("TEST").and()
+                .rememberMe().rememberMeServices(rememberMeServices()).userDetailsService(customUserService()).key("TEST").and()
                 .authorizeRequests()
                 .antMatchers("/login", "/register").permitAll()
                 .anyRequest()
